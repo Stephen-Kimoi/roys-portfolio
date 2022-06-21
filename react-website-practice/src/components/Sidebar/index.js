ee04e3efb1c7feb0ca1d90d97e33c8afcaa2990e
Steve Kimoi <moenisteve@gmail.com>
2022-06-21 11:28:07 +0300

    creating the home page

--- react-website-practice/src/components/Sidebar/index.js
+++ react-website-practice/src/components/Sidebar/index.js
@@ -18,19 +18,19 @@ const Sidebar = ({isOpen, toggle}) => {
         </Icon> 
         <SidebarWrapper>
             <SidebarMenu>
-                <SidebarLink to="about">
+                <SidebarLink to="about" onClick={toggle}>
                     About
                 </SidebarLink> 
-                <SidebarLink to="discover">
+                <SidebarLink to="discover" onClick={toggle}>
                     Discover
                 </SidebarLink>
-                <SidebarLink to="services">
+                <SidebarLink to="services" onClick={toggle}>
                     Services 
                 </SidebarLink>
-                <SidebarLink to="contact">
+                <SidebarLink to="contact" onClick={toggle}>
                     Contact
                 </SidebarLink> 
-                <SidebarLink to="signup">
+                <SidebarLink to="signup" onClick={toggle}>
                     Sign up 
                 </SidebarLink>
             </SidebarMenu> 
